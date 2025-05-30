0bc020c2e50f7d161e9ee2bb573e835839d7f537
MIKELSOUTH <mbsingenieria2024@gmail.com>
2025-05-29 21:28:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Dirección IP de la Raspberry Pi donde se enviarán los datos del QR
-RASPBERRY_PI_URL = https://1e12-191-126-129-222.ngrok-free.app/guardar_qr
+RASPBERRY_PI_URL = 'https://1e12-191-126-129-222.ngrok-free.app/guardar_qr'
 
 # -----------------------------------------------
 # *Ruta raíz para verificar que el servidor está activo*
